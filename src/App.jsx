821a5cbd073c9892b52a67cd4c52129c016ef7c7
amidshojaeinasab <amidshojaeinasab@gmail.com>
2025-08-31 12:20:53 +0330

    like books

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Layout from "./layouts/layout";
+import Books from "./components/Books";
+
+function App() {
+  return (
+    <>
+      <Layout>
+      <Books/>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,19 @@
+﻿import styles from "./Layout.module.css"
+function Layout({children}) {
+  return (
+    <>
+    <header className={styles.header}>
+      <h1>Book App</h1>
+      <p>
+        <a href="https://shojaeinasab.ir/">Shojaeinasab</a>
+      </p>
+    </header>
+    {children}
+    <footer className={styles.footer}>
+      <p>Developed by Shojaeinasab</p>
+    </footer>
+    </>
+  )
+}
+
+export default Layout
