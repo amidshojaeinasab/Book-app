83057483358351429d21daa69c9284884e86812b
amidshojaeinasab <amidshojaeinasab@gmail.com>
2025-09-03 19:23:28 +0330

    search books

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,11 +1,16 @@
 ﻿import { useState } from "react"
-import {books} from "../constants/mockData"
+import {books as bookData} from "../constants/mockData"
 import BookCard from "./BookCard"
 import SideCard from "./SideCard"
 
 import styles from "./Books.module.css"
+import SearchBox from "./SearchBox"
+
+
 function Books() {
+  const [books, setbooks] = useState(bookData)
   const [liked, setliked] = useState([])
+  const [search, setsearch] = useState([])
 
   const handleLikeedList =(book, status) =>{
     if (status){
@@ -14,8 +19,20 @@ function Books() {
     }else{
       setliked((liked) => [...liked,book])
     }
+}
+const searchHandler = () =>{
+      if(search){
+      const newBooks = bookData.filter((book) =>
+      book.title.toLocaleLowerCase().includes(search)
+      );
+      setbooks(newBooks);
+    }else{
+      setbooks(bookData);
+    }
   }
   return (
+   <>
+   <SearchBox searchHandler={searchHandler} search={search} setsearch={setsearch}/>
     <div className={styles.container}>
         <div className={styles.cards}>
         {books.map((book)=>(
@@ -29,6 +46,7 @@ function Books() {
         ))}</div>
         }
     </div>
+   </>
   )
 }
 
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,14 @@
+﻿import { IoSearchSharp } from "react-icons/io5"
+import styles from "./SearchBox.module.css"
+function SearchBox({search , setsearch , searchHandler}) {
+  return (
+    <div className={styles.search}>
+        <input type="text" placeholder="Search title" value={search} onChange={(e)=> setsearch (e.target.value.toLocaleLowerCase())} />
+        <button onClick={searchHandler}>
+            <IoSearchSharp/>
+        </button>
+    </div>
+  )
+}
+
+export default SearchBox
